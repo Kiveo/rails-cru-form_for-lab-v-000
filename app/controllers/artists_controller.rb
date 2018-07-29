2809267ac9e503add7b599c441e0ed863617adfc
Maverick Tesini <mavericktesini@gmail.com>
2018-07-29 22:37:31 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,35 @@
-class ArtistsController < ApplicationController
+class artistsController < ApplicationController
   def index
-    @artists = Artist.all
+    @artists = artist.all
   end
 
   def new
     @artist = Artist.find(params[:id])
   end
 
+  def create
+    @artist = artist.create(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+  def artist_params(*args)
+    params.require(:artist)
+  end
 
 end
