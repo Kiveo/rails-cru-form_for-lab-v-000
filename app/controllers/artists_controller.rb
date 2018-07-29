bf61a831628f0aaa15c2d90ac026b4db56cb172b
Maverick Tesini <mavericktesini@gmail.com>
2018-07-29 23:26:58 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -29,7 +29,7 @@ class ArtistsController < ApplicationController
   private
 
   def artist_params(*args)
-    params.require(:artist).permit(:name, :bio)
+    params.require(:artist).permit(*args)
   end
 
 end
