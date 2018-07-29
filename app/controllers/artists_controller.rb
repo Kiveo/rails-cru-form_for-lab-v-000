cd574dd4567a624ab463a037c34739f393c30d13
Maverick Tesini <mavericktesini@gmail.com>
2018-07-29 23:24:42 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -22,7 +22,7 @@ class ArtistsController < ApplicationController
 
   def update
     @artist = Artist.find(params[:id])
-    @artist.update(artist_params(:artist))
+    @artist.update(artist_params)
     redirect_to artist_path
   end
 
