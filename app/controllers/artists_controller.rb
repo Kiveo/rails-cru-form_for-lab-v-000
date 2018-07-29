d683a8e5decab2a4b99514b09f570ff4ec75f799
Maverick Tesini <mavericktesini@gmail.com>
2018-07-29 23:28:24 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,7 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    @artist = Artist.create(artist_params)
+    @artist = Artist.create(artist_params(:name, :bio))
     redirect_to artist_path(@artist)
   end
 
